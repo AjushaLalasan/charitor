8cff4d9211e49d0aa3308dc00333efa351192c6b
ajusha <vlajusha@gmail.com>
2025-04-04 11:57:35 +0530

    commit

---
+++ src/main/java/com/charitor/CharitorApplication.java
@@ -0,0 +1,13 @@
+package com.charitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitorApplication.class, args);
+	}
+
+}
